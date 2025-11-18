d6adebe12701490581ed5610a9a3293a3a0902c8
Adeel Khan <adeel.khan@A006-01817.local>
2025-11-19 00:12:05 +0500

    Feat:added the tabs for each screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const Tab = createBottomTabNavigator();
+const RootStack = createNativeStackNavigator();
+
+const Card = ({ text }: { text: string }) => (
+  <View style={styles.container}>
+    <Text style={styles.text}>{text}</Text>
+  </View>
+);
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Main" options={{ headerShown: false }}>
+          {() => (
+            <Tab.Navigator screenOptions={{
+              headerShown: true,
+              tabBarIcon: () => null,
+              tabBarStyle: {
+                borderTopWidth: 0,
+
+              },
+              tabBarLabelStyle: {
+                fontSize: 14,
+                textAlign: 'center',  
+              }
+            }}
+            >
+              <Tab.Screen name="Feed" component={() => <Card text="FeedStack" />} />
+              <Tab.Screen name="Search" component={() => <Card text="SearchStack" />} />
+              <Tab.Screen name="Camera" component={() => <Card text="CameraStack" />} />
+              <Tab.Screen name="Favorites" component={() => <Card text="FavoritesStack" />} />
+              <Tab.Screen name="Profile" component={() => <Card text="ProfileStack" />} />
+            </Tab.Navigator>
+          )}
+        </RootStack.Screen>
+
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
   },
 });
